d25d67cf7a7350173902ba8ac1590b12e153ebb2
Jorss_ <jorss_@vl23h108.fciencias.unam.mx>
2019-10-08 13:26:33 -0400

    mini main de prueba

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+
+    print("-------------------"'\n' + "Bienvenido al Menu" + '\n'"-------------------")
+    menu_opciones()
+def menu_opciones():
+    print("1 : Registrar nuevo Jugador")
+    print("2 : Iniciar Sesion")
+    teclado = input("Escoga la funcion que quiere hacer : ")
+    if(teclado == 1):
+        print("aqui va lo que tiene que hacer")
+    if(teclado == 2):
+        print("aqui va lo que tiene que hacer 2")
+
+
+if __name__ == '__main__':
+    print main()
