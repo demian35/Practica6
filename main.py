d037ef929d3739bbeca4659c557e357daa067130
demian35 <josedemian@ciencias.unam.mx>
2019-10-20 18:21:13 -0500

    Un avance de la implementacion del menu

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def main():
+from pymysql import BasedeDatosJuego
+import hashlib
+
+
+##def main():
 
     print("-------------------"'\n' + "Bienvenido al Menu" + '\n'"-------------------")
     menu_opciones()
@@ -7,10 +11,23 @@ def menu_opciones():
     print("2 : Iniciar Sesion")
     teclado = input("Escoga la funcion que quiere hacer : ")
     if(teclado == 1):
-        print("aqui va lo que tiene que hacer")
+         nombrenuevo = input("Indique cual sera su nombre de usuario : ")
+        passwordnuevo = input("Indique cual sera su contrasena : ")
+        #sql = "INSERT INTO Jugador (nombrenuevo,passwordnuevo) VALUES ('{}' ,'{}')".format(nombre,password)
+        #self.cursor.execute(sql)
+        #self.connection.commit()
+        registronew= BasedeDatosJuego().agregarUsuario(nombrenuevo,passwordnuevo)
     if(teclado == 2):
-        print("aqui va lo que tiene que hacer 2")
+        usuario= input("Teclee su nombre de usuario : ")
+        clave= input("Teclee su contrasena : ") 
+        if usuario in BasedeDatosJuego:
+            if clave in BasedeDatosJuego:
+                return 1 
+            else:
+                print("Este usuario no existe : ")
+        else:
+            return 2 
 
 
-if __name__ == '__main__':
-    print main()
+##if __name__ == '__main__':
+##    print main()
